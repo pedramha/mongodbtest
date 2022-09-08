4fea0bd470b487647f3005058677ace7c6e3d238
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-09-08 09:27:14 +0200

    test output

--- ouput.tf
+++ ouput.tf
@@ -5,6 +5,9 @@ output "connection_strings" {
 output "connection_strings2" {
   value = mongodbatlas_cluster.cluster.connection_strings
 }
+output "connstring3" {
+    value = "mongodb+srv://${mongodbatlas_database_user.user.username}:${mongodbatlas_database_user.user.username}${trim(mongodbatlas_cluster.cluster.connection_strings[0].standard_srv, "mongodb+srv://")}"
+}
 
 output "user1" {
   value = mongodbatlas_database_user.user.username
