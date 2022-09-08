b389a764b5d3c029a44e935ea1bbcf33422a7271
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-09-08 09:30:13 +0200

    amended connection string

--- ouput.tf
+++ ouput.tf
@@ -1,16 +1,5 @@
 output "connection_strings" {
-  value = mongodbatlas_cluster.cluster.connection_strings[0].standard_srv
-}
-
-output "connection_strings2" {
-  value = mongodbatlas_cluster.cluster.connection_strings
-}
-output "connstring3" {
-    value = "mongodb+srv://${mongodbatlas_database_user.user.username}:${mongodbatlas_database_user.user.username}${trim(mongodbatlas_cluster.cluster.connection_strings[0].standard_srv, "mongodb+srv://")}"
-}
-
-output "user1" {
-  value = mongodbatlas_database_user.user.username
+    value = "mongodb+srv://${mongodbatlas_database_user.user.username}:${mongodbatlas_database_user.user.password}@${trim(mongodbatlas_cluster.cluster.connection_strings[0].standard_srv, "mongodb+srv://")}"
 }
 
 output "ipaccesslist" {
