4667314322bda45c316bf8ea8a912754197d912e
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-09-08 09:00:56 +0200

    amended vars

--- variables.tf
+++ variables.tf
@@ -46,3 +46,7 @@ variable "ip_address" {
   type        = string
   description = "The IP address that the cluster will be accessed from, can also be a CIDR range or AWS security group"
 }
+variable "cidr" {
+  type        = string
+  description = "The CIDR range or AWS security group"
+}
